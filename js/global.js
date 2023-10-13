6958900db56a4d596c012550633b23af56506743
Brian Bauska <brianbauska@gmail.com>
2023-10-12 22:24:14 -0700

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,28 @@
+/* global.js - theme by: brian a. bauska (grandpa) */
+$( function() {
+  // add background image - 4rd countdown to 14th birthday 10/11/2024.
+  $.backstretch('images/Silus-and-Kitterson.jpg');
+  // date of birth: 10/11/10.
+  var endDate = "Oct  11, 2024 23:59:59";
+  $('.countdown.simple').countdown({ date: endDate });
+  $('.countdown.styled').countdown({
+    date: endDate,
+    render: function(data) {
+      $(this.el).html("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div><div>" 
+        + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" 
+        + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" 
+        + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+    }
+  });
+  $('.countdown.callback').countdown({
+    date: +(new Date) + 10000,
+    render: function(data) {
+      $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
+    },
+    onEnd: function() {
+            $(this.el).addClass('ended');
+    }
+  }).on("click", function() {
+    $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
+  });
+});
