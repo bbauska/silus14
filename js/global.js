a690619e7237f7aaff2de04b990c2ab35975df71
Brian Bauska <brianbauska@gmail.com>
2024-08-27 00:54:30 -0700

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 /* global.js - theme by: brian a. bauska (grandpa) */
 $( function() {
   // add background image - 3rd countdown to 14th birthday 10/11/2024. Set to 10/12 @3:59:59am. Started with 11th birthday on 10/11/21.
-  $.backstretch('images/Silus-and-Kitterson-2000.jpg');  /* -500 */
+  $.backstretch('images/July-2-2024-kitty-2.png');  /* -500 */
   // date of birth: 10/11/10.
   var endDate = "Oct  12, 2024 3:59:59";
   $('.countdown.simple').countdown({ date: endDate });
